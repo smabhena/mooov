3dc8fed22ecbdc7305334ba0eb453e9cdf2f8701
Sino <100207221+smabhena@users.noreply.github.com>
2022-02-28 09:42:47 +0200

    Added landing page
    Created landing page which is viewed after logging in to the app

--- Mooov/Mooov/Controllers/LogInViewController.swift
+++ Mooov/Mooov/Controllers/LogInViewController.swift
@@ -14,16 +14,21 @@ class LogInViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     
     @IBAction private func loginButtonPressed(_ sender: Any) {
-        
         if usernameField.text == "Admin" && passwordField.text == "TestPass123"{
-            print("Successful log in")
+            let vc = storyboard?.instantiateViewController(identifier: "landingPageVC") as? LandingPageViewController
+            
+            guard let vc = vc else {
+                return
+            }
+            
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true, completion: nil)
+            
         } else {
             self.displayAlert(title: "Log in failed",
                               message: "Enter correct username or password",
                               buttonTitle: "Ok")
-            print("Log in failed")
         }
-        
     }
     
     override func viewDidLoad() {
