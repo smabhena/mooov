e122740f27204a29c60bf12da1fdc7454b3f571d
Sino <100207221+smabhena@users.noreply.github.com>
2022-04-11 07:37:59 +0200

    Encapsulated mock classes in HomeTests class

--- Mooov/MooovTests/HomeTests/HomeTests.swift
+++ Mooov/MooovTests/HomeTests/HomeTests.swift
@@ -38,71 +38,73 @@ class HomeTests: XCTestCase {
     func testMovie() {
         XCTAssertEqual(viewModel.movie(atIndex: 0)?.title, "Superman")
     }
-}
-
-class MockDelegate: HomePageViewModelDelegate {
-    var showErrorCalled = false
-    var showReloadViewCalled = false
     
-    func showError(error: String) {
-        showErrorCalled = true
+    class MockDelegate: HomePageViewModelDelegate {
+        var showErrorCalled = false
+        var showReloadViewCalled = false
+        
+        func showError(error: String) {
+            showErrorCalled = true
+        }
+        func reloadView() {}
     }
-    func reloadView() {}
-}
 
-class MockRepository: MovieRepositoryType {
-    var shouldFail = false
+    class MockRepository: MovieRepositoryType {
+        var shouldFail = false
 
-    let movie: MovieInfo = MovieInfo(title: "Superman",
-                                     year: "2020",
-                                     rated: "8/10",
-                                     released: "2020",
-                                     runtime: "127min",
-                                     genre: "action",
-                                     director: "Zac Snyder",
-                                     writer: "Zac Synder",
-                                     actors: "Some one",
-                                     plot: "A plot",
-                                     language: "English",
-                                     country: "USA",
-                                     awards: "None",
-                                     poster: "Poster",
-                                     metascore: "4/5",
-                                     imdbRating: "9/10",
-                                     imdbVotes: "352",
-                                     imdbId: "t12341234",
-                                     type: "Movie",
-                                     dvd: "dvd",
-                                     boxOffice: "boxoffice",
-                                     production: "prod",
-                                     website: "website",
-                                     response: "response",
-                                     rating: [Rating(
-                                        source: "source",
-                                        value: "value")])
-    
-    let result: SearchResults = SearchResults(search: [Movie(title: "Batman",
-                                                             year: "2022",
-                                                             imdbId: "9/10",
-                                                             type: "Movie",
-                                                             poster: "Poster")],
-                                              totalResults: "1",
-                                              response: "true")
-    
-    func fetchSearchResults(_ searchTitle: String, completion: @escaping (SearchMovieResult)) {
-        if shouldFail {
-            completion(.failure(.serverError))
-        } else {
-            completion(.success(result))
+        let movie: MovieInfo = MovieInfo(title: "Superman",
+                                         year: "2020",
+                                         rated: "8/10",
+                                         released: "2020",
+                                         runtime: "127min",
+                                         genre: "action",
+                                         director: "Zac Snyder",
+                                         writer: "Zac Synder",
+                                         actors: "Some one",
+                                         plot: "A plot",
+                                         language: "English",
+                                         country: "USA",
+                                         awards: "None",
+                                         poster: "Poster",
+                                         metascore: "4/5",
+                                         imdbRating: "9/10",
+                                         imdbVotes: "352",
+                                         imdbId: "t12341234",
+                                         type: "Movie",
+                                         dvd: "dvd",
+                                         boxOffice: "boxoffice",
+                                         production: "prod",
+                                         website: "website",
+                                         response: "response",
+                                         rating: [Rating(
+                                            source: "source",
+                                            value: "value")])
+        
+        let result: SearchResults = SearchResults(search: [Movie(title: "Batman",
+                                                                 year: "2022",
+                                                                 imdbId: "9/10",
+                                                                 type: "Movie",
+                                                                 poster: "Poster")],
+                                                  totalResults: "1",
+                                                  response: "true")
+        
+        func fetchSearchResults(_ searchTitle: String, completion: @escaping (SearchMovieResult)) {
+            if shouldFail {
+                completion(.failure(.serverError))
+            } else {
+                completion(.success(result))
+            }
+            
         }
         
-    }
-    
-    func fetchMovie(_ movieID: String, completion: @escaping (FetchMovie)) {
-        if shouldFail {
-            completion(.failure(.serverError))
-        } else {
-            completion(.success(movie))
+        func fetchMovie(_ movieID: String, completion: @escaping (FetchMovie)) {
+            if shouldFail {
+                completion(.failure(.serverError))
+            } else {
+                completion(.success(movie))
+            }
         }
     }
 }
+
+
